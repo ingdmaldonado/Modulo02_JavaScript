c84911b8f47475385f085b5a335534a94270e00d
ingdmaldonado <ingdmaldonado@gmail.com>
2024-08-22 13:32:29 -0300

    cambios hoy

--- Capitulo000_ClaseOnline/ClasesNODO_CO01/Clase10Borrador/controlador.js
+++ Capitulo000_ClaseOnline/ClasesNODO_CO01/Clase10Borrador/controlador.js
@@ -42,14 +42,29 @@ window.addEventListener("load",()=>{
     const idBtnCotizar = document.querySelector("#idBtnCotizar");
 
 
+    const idMensajeFinanciacion = document.querySelector("#idMensajeFinanciacion");
+    const idDivTotalAFinanciar = document.querySelector("#idDivTotalAFinanciar");
+
+    const idDivPlanFinanciacion = document.querySelector("#idDivPlanFinanciacion");
+    const idSelectorPlanFinanciacion = document.querySelector("#idSelectorPlanFinanciacion");
+    const idResultadoFinanciacion = document.querySelector("#idResultadoFinanciacion");
+
     console.log(idSelectorVehiculo);
     console.log(idDineroEntregado);
     console.log(idBtnCotizar);
+    console.log(idMensajeFinanciacion);
+    console.log(idDivTotalAFinanciar);
+    console.log(idSelectorPlanFinanciacion);
+    console.log(idResultadoFinanciacion);
+
+    idDivTotalAFinanciar.style.display = 'none';
+    idDivPlanFinanciacion.style.display = 'none';
+
+
 
     idBtnCotizar.addEventListener("click",()=>
         {
-            console.log("calcular la cotizacion");
-
+          
             let vehiculoElegido = Number(idSelectorVehiculo.value);
             console.log(vehiculoElegido);
 
@@ -59,9 +74,44 @@ window.addEventListener("load",()=>{
             let dineroAEntregar = Number(idDineroEntregado.value);
             console.log(dineroAEntregar);
 
+            let calculoDel50PorCientoValorVehiculo = valorVehiculoElegido * 50/100;
 
+            let diferenciaAFinanciar = 0;
+            
+            if(dineroAEntregar <= 0)
+            {
+                alert(`por favor, Indique el dinero a entregar`);
+            }
+            else
+            {
+                if (dineroAEntregar < calculoDel50PorCientoValorVehiculo)
+                {
+                    alert(`la entrega que Ud. realizará debe ser mayor al 50% del valor del vehículo por políticas de la empresa. es decir mayor a ${calculoDel50PorCientoValorVehiculo.toLocaleString('ES-es')}`);
+                }
+                else
+                {
+                    if(dineroAEntregar > valorVehiculoElegido)
+                    {
+                        alert(`la entrega no puede ser mayor al valor total del vehículo y su entrega es = ${dineroAEntregar.toLocaleString("ES-es")} y el valor del vehículo es = ${valorVehiculoElegido.toLocaleString("ES-es")}`);
+                    }
+                    else
+                    {
+                        console.log("calcular la cotizacion");
+                        diferenciaAFinanciar = valorVehiculoElegido - dineroAEntregar;
+                        idDivTotalAFinanciar.style.display = 'block';
+                        idDivPlanFinanciacion.style.display = 'block';
+                        idMensajeFinanciacion.textContent = `Total del vehículo ${valorVehiculoElegido.toLocaleString("Es-es")} - Dinero a entregar en efectivo ${dineroAEntregar.toLocaleString("ES-es")} - Resto a Financiar = ${diferenciaAFinanciar.toLocaleString("ES-es")}`;
+                    }
+                }
+            }
         })
 
+        idSelectorPlanFinanciacion.addEventListener("change",()=>
+            {
+                console.log("estan cambiando el selector de planes de financiacion");
+                idResultadoFinanciacion.textContent = `El Total Financiado es de 18.500.000`;
+            })
+    
 
     console.log("application is running");
 
