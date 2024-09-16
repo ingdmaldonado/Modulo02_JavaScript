cbe0ede9433faa26ea8cca9919b227deeaeed627
Daniel Maldonado <ingdmaldonado@gmail.com>
2024-09-16 11:44:07 -0300

    cambios arca 16.09.2024

--- Capitulo000_Documento/04_ProyectosVaciosParaTrabajar_VECTORES/PadronDeAfip/vista00_procesosVarios.js
+++ Capitulo000_Documento/04_ProyectosVaciosParaTrabajar_VECTORES/PadronDeAfip/vista00_procesosVarios.js
@@ -1,35 +1,51 @@
 
-//import vectorContribuyentes from "./Datos/VectorContribuyentes_10000";
+import vectorContribuyentes from "./Datos/VectorContribuyentes_10000.js";
 //import vectorContribuyentes from "./Datos/VectorContribuyentes_25000.js";
-import vectorContribuyentes from "./Datos/VectorContribuyentes_50000.js";
+//import vectorContribuyentes from "./Datos/VectorContribuyentes_50000.js";
 //import vectorContribuyentes from ".Datos//VectorContribuyentes_100000.js";
 //import vectorContribuyentes from ".Datos/VectorContribuyentes_200000.js";
 
 
 window.addEventListener("load",()=>{
 
+    console.clear();
     console.log("andando");
 
   
+    const idBtnLimpiarConsola = document.querySelector("#idBtnLimpiarConsola");
     const idBtnMostrarTodos = document.querySelector("#idBtnMostrarTodos"); 
     const idBtnBuscarPorNombre = document.querySelector("#idBtnBuscarPorNombre");   
     const idBtnBuscarPorNombreyExentoEnIVA = document.querySelector("#idBtnBuscarPorNombreyExentoEnIVA");
     const idBtnFiltrarPorNombreYExentosEnIVA = document.querySelector("#idBtnFiltrarPorNombreYExentosEnIVA");
     const idBtnFiltrosVarios = document.querySelector("#idBtnFiltrosVarios");
+    const idBtnCantidadDeMonotributistas = document.querySelector("#idBtnCantidadDeMonotributistas");
+    const idCantidadCuitsPorTerminacion = document.querySelector("#idCantidadCuitsPorTerminacion");
 
     console.log(idBtnMostrarTodos);
     console.log(idBtnBuscarPorNombre);
     console.log(idBtnBuscarPorNombreyExentoEnIVA);
     console.log(idBtnFiltrarPorNombreYExentosEnIVA);
 
+    idBtnLimpiarConsola.addEventListener("click",()=>{
+
+        console.clear();
+
+    })
+
+    /********************************************************************/
     /* (01) boton que me permite mostrar todos los elementos del vector */
+    /********************************************************************/
+
     idBtnMostrarTodos.addEventListener("click",()=>{
 
         console.log(vectorContribuyentes);
 
     })
 
-    /* (02) boton que me permite buscar por nombre */
+    /*********************************************************/
+    /* (02) boton que me permite buscar por nombre (ALBERTO) */
+    /*********************************************************/
+
     idBtnBuscarPorNombre.addEventListener("click",()=>{
 
        vectorContribuyentes.forEach(element => {
@@ -40,7 +56,10 @@ window.addEventListener("load",()=>{
        })
     })
 
-     /* (03) boton que me permite buscar por nombre y si se encuentra exento en el impuesto de IVA */
+    /*********************************************************************************************/
+    /* (03) boton que me permite buscar por nombre y si se encuentra exento en el impuesto de IVA */
+    /**********************************************************************************************/
+
     idBtnBuscarPorNombreyExentoEnIVA.addEventListener("click",()=>{
 
         vectorContribuyentes.forEach(element => {
@@ -52,7 +71,15 @@ window.addEventListener("load",()=>{
         })
     })
 
-    /* (04) boton que me permite FILTRAR los "ALBERTO" que no INTEGRAN SOCIEDADES y ESTAN EXENTOS EN IVA */
+    /******************************************************************************************************/
+    /* (04) boton que me permite FILTRAR los "ALBERTO" que no INTEGRAN SOCIEDADES y ESTAN EXENTOS EN IVA  */
+    /******************************************************************************************************/
+    /*
+        - Son Empleadores
+        - No integran Sociedades
+        - Estan Exentos en IVA
+    */
+
     idBtnFiltrarPorNombreYExentosEnIVA.addEventListener("click",()=>{
 
         const vectorDeEmpleadores = vectorContribuyentes.filter(element => element.PadronAFIPEmpleador === 'S' && element.PadronAFIPIntegranteSoc ==='N' && element.PadronAFIPImpIVA ==='EX')
@@ -62,31 +89,35 @@ window.addEventListener("load",()=>{
     })
 
  
-    /* (05) Filtrar todas las Empresas que:
+    /******************************************/
+    /* (05) Filtrar todas las Empresas que:   */
+    /******************************************/
     
+    /*
         - el Cuit termina en 9
         - cuyo nombre no contenga la palabra SUCESION
-        - 
-    
-    cuyo Cuit termina en 9 */
+        - que no integren sociedades.
 
-    idBtnFiltrosVarios.addEventListener("click",()=>{
+        cuyo Cuit termina en 9 
+    */
+
+    idBtnFiltrosVarios.addEventListener("click",()=>
+        {
 
         const VectorResultado = vectorContribuyentes.filter(element => 
             {
-
-                console.log(`PadronAFIPId: ${element.PadronAFIPId}`);
-                console.log(`PadronAFIPCUIT: ${element.PadronAFIPCUIT}`);
-                console.log(`PadronAFIPDenominacion: ${element.PadronAFIPDenominacion} `);
-                console.log(`PadronAFIPImpGanancias: ${element.PadronAFIPImpGanancias}`);
-                console.log(`PadronAFIPImpIVA: ${element.PadronAFIPImpIVA}`);
-                console.log(`PadronAFIPMonotributo: ${element.PadronAFIPMonotributo}`);
-                console.log(`PadronAFIPIntegranteSoc: ${element.PadronAFIPIntegranteSoc}`);
-                console.log(`PadronAFIPEmpleador: ${element.PadronAFIPEmpleador}`);
-                console.log(`PadronAFIPActividadMonotributo: ${element.PadronAFIPActividadMonotributo}`);           
-                console.log(element.PadronAFIPCUIT.substring(10));// este me esta mostrando bien los datos                
                 if((element.PadronAFIPCUIT.substring(10) === '9') && !(element.PadronAFIPDenominacion.includes('SUCESION')) && (element.PadronAFIPEmpleador === 'S'))
                     {
+                        console.log(`PadronAFIPId: ${element.PadronAFIPId}`);
+                        console.log(`PadronAFIPCUIT: ${element.PadronAFIPCUIT}`);
+                        console.log(`PadronAFIPDenominacion: ${element.PadronAFIPDenominacion} `);
+                        console.log(`PadronAFIPImpGanancias: ${element.PadronAFIPImpGanancias}`);
+                        console.log(`PadronAFIPImpIVA: ${element.PadronAFIPImpIVA}`);
+                        console.log(`PadronAFIPMonotributo: ${element.PadronAFIPMonotributo}`);
+                        console.log(`PadronAFIPIntegranteSoc: ${element.PadronAFIPIntegranteSoc}`);
+                        console.log(`PadronAFIPEmpleador: ${element.PadronAFIPEmpleador}`);
+                        console.log(`PadronAFIPActividadMonotributo: ${element.PadronAFIPActividadMonotributo}`);           
+                       // console.log(element.PadronAFIPCUIT.substring(10));// este me esta mostrando bien los datos                
                         return true;
                     } 
             }
@@ -96,5 +127,88 @@ window.addEventListener("load",()=>{
     })
 
   
+    idBtnCantidadDeMonotributistas.addEventListener("click",()=>
+        {
+          
+            alert("arrancando");
+            vectorContribuyentes.forEach(element => {
+
+                /*
+                console.log(`PadronAFIPId: ${element.PadronAFIPId}`);
+                console.log(`PadronAFIPCUIT: ${element.PadronAFIPCUIT}`);
+                console.log(`PadronAFIPDenominacion: ${element.PadronAFIPDenominacion} `);
+                console.log(`PadronAFIPImpGanancias: ${element.PadronAFIPImpGanancias}`);
+                console.log(`PadronAFIPImpIVA: ${element.PadronAFIPImpIVA}`);
+                console.log(`PadronAFIPMonotributo: ${element.PadronAFIPMonotributo}`);
+                console.log(`PadronAFIPIntegranteSoc: ${element.PadronAFIPIntegranteSoc}`);
+                console.log(`PadronAFIPEmpleador: ${element.PadronAFIPEmpleador}`);
+                console.log(`PadronAFIPActividadMonotributo: ${element.PadronAFIPActividadMonotributo}`);           
+                */
+
+
+                if(element.PadronAFIPMonotributo != 'NI' && element.PadronAFIPEmpleador ==='S')
+                {
+                    console.log(element);
+                }
+
+            })
+
+            alert("terminando");
+
+
+            let Resultado1 = vectorContribuyentes.reduce((contadorMonotributistas, element) => {
+                if (element.PadronAFIPMonotributo != 'NI' && element.PadronAFIPEmpleador ==='S') {
+                    return contadorMonotributistas + 1; // Incrementar el contador si cumple la condición
+                } else {
+                    return contadorMonotributistas; // Mantener el valor actual si no cumple la condición
+                }
+            }, 0); // Inicializar el contador en 0
+            
+            console.log(Resultado1); // Debería mostrar el número de monotributistas que cumplen la condición
+            
+
+            let Resultado2 = vectorContribuyentes.reduce((contador,element)=>{
+
+                if(element.PadronAFIPMonotributo != 'NI' && element.PadronAFIPEmpleador ==='S')
+                {
+                    return contador + 1;
+                }
+                else
+                {
+                    return contador;
+                }
+
+            },0);
+
+            console.log(Resultado2);
+
+
+        
+        })
+
+    idCantidadCuitsPorTerminacion.addEventListener("click",()=>{
+
+        console.log("funcionando");
+
+       
+        let Resultado = vectorContribuyentes.reduce((objetoAcumulador,element)=>{
+
+            if(element.PadronAFIPCUIT.substring(10)==='1')
+            {
+                objetoAcumulador.cantidad1 = objetoAcumulador.cantidad1 + 1;
+            }
+            if(element.PadronAFIPCUIT.substring(10)==='2')
+            {
+                objetoAcumulador.cantidad2 = objetoAcumulador.cantidad2 + 1;
+            }
+
+            return objetoAcumulador;
+
+        },{cantidad1:0,cantidad2:0});
+
+        console.log(Resultado);
+
+
+    })
 
 })
