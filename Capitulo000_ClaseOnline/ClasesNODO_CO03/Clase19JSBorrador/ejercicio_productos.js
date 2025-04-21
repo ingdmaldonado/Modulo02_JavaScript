abd77c46445dedb003fea224e9942c6764120f9c
Daniel Maldonado <ingdmaldonado@gmail.com>
2025-04-21 18:12:45 -0300

    cambios en casa

--- Capitulo000_ClaseOnline/ClasesNODO_CO03/Clase19JSBorrador/ejercicio_productos.js
+++ Capitulo000_ClaseOnline/ClasesNODO_CO03/Clase19JSBorrador/ejercicio_productos.js
@@ -1,92 +1,262 @@
 
+
+
+
 /* Dado un Vector de Productos como el de a continuación 
 
     realizar:
 
-    1) Recorrer el vector con forEach y mostrar por consola los 
-    productos que tienen un precio mayor a 2500 pesos
+    */
 
-    2) Recorrer el vector con forEach y obtener el precio 
-    promedio de los 
-        LACTEOS
-        BEBIDAS     
+    const productos = [
+      { codigo: 1001, descripcion: "LECHE ENTERA 1L", categoriaNombre: "LACTEOS", precio: 1500.50 },
+      { codigo: 1002, descripcion: "QUESO CREMA 300G", categoriaNombre: "LACTEOS", precio: 2500.20 },
+      { codigo: 1003, descripcion: "YOGUR BEBIBLE FRUTILLA", categoriaNombre: "LACTEOS", precio: 900.00 },
     
-    3) Recorrer el vector que tiene los productos y generar un segundo vector
-    que podría llamarse ResumenDeProductos que tenga para cada categoria
-    la cantidad de productos, el precio promedio, el precio mínimo y el precio máximo de
-    esa categoria 
-*/
-
-
-
-const productos = [
-    { codigo: 1001, descripcion: "LECHE ENTERA 1L", categoria: "LACTEOS", precio: 1500.50 },
-    { codigo: 1002, descripcion: "QUESO CREMA 300G", categoria: "LACTEOS", precio: 2500.20 },
-    { codigo: 1003, descripcion: "YOGUR BEBIBLE FRUTILLA", categoria: "LACTEOS", precio: 900.00 },
+      { codigo: 2001, descripcion: "AGUA MINERAL 1.5L", categoriaNombre: "BEBIDAS", precio: 1250.20 },
+      { codigo: 2002, descripcion: "GASEOSA COLA 2.25L", categoriaNombre: "BEBIDAS", precio: 2850.50 },
+      { codigo: 2003, descripcion: "JUGO EN POLVO NARANJA", categoriaNombre: "BEBIDAS", precio: 500.00 },
+    
+      { codigo: 3001, descripcion: "PAN FRANCÉS", categoriaNombre: "PANIFICADOS", precio: 2000.00 },
+      { codigo: 3002, descripcion: "FACTURA DE DULCE DE LECHE", categoriaNombre: "PANIFICADOS", precio: 6800 },
+      { codigo: 3003, descripcion: "GALLETITAS SALADAS 250G", categoriaNombre: "PANIFICADOS", precio: 1250 },
+    
+      { codigo: 4001, descripcion: "ARROZ LARGO FINO 1KG", categoriaNombre: "ALMACEN", precio: 5420 },
+      { codigo: 4002, descripcion: "AZÚCAR 1KG", categoriaNombre: "ALMACEN", precio: 1200 },
+      { codigo: 4003, descripcion: "ACEITE GIRASOL 900ML", categoriaNombre: "ALMACEN", precio:4820.50 },
   
-    { codigo: 2001, descripcion: "AGUA MINERAL 1.5L", categoria: "BEBIDAS", precio: 1250.20 },
-    { codigo: 2002, descripcion: "GASEOSA COLA 2.25L", categoria: "BEBIDAS", precio: 2850.50 },
-    { codigo: 2003, descripcion: "JUGO EN POLVO NARANJA", categoria: "BEBIDAS", precio: 500.00 },
+      { codigo: 5001, descripcion: "LAVANDINA 1L", categoriaNombre: "LIMPIEZA", precio: 950 },
+      { codigo: 5002, descripcion: "DETERGENTE LIMÓN 750ML", categoriaNombre: "LIMPIEZA", precio: 1100 },
   
-    { codigo: 3001, descripcion: "PAN FRANCÉS", categoria: "PANIFICADOS", precio: 2000.00 },
-    { codigo: 3002, descripcion: "FACTURA DE DULCE DE LECHE", categoria: "PANIFICADOS", precio: 6800 },
-    { codigo: 3003, descripcion: "GALLETITAS SALADAS 250G", categoria: "PANIFICADOS", precio: 1250 },
+      { codigo: 6001, descripcion: "SHAMPOO SUAVE 400ML", categoriaNombre: "PERFUMERIA", precio: 2200 },
+      { codigo: 6002, descripcion: "JABÓN DE TOCADOR X3", categoriaNombre: "PERFUMERIA", precio: 1850 },
   
-    { codigo: 4001, descripcion: "ARROZ LARGO FINO 1KG", categoria: "ALMACEN", precio: 5420 },
-    { codigo: 4002, descripcion: "AZÚCAR 1KG", categoria: "ALMACEN", precio: 1200 },
-    { codigo: 4003, descripcion: "ACEITE GIRASOL 900ML", categoria: "ALMACEN", precio:4820.50 },
+      { codigo: 7001, descripcion: "HAMBURGUESAS CONGELADAS X4", categoriaNombre: "CONGELADOS", precio: 5200 },
+      { codigo: 7002, descripcion: "VEGETALES MIX CONGELADOS 500G", categoriaNombre: "CONGELADOS", precio: 3600 },
+  
+      { codigo: 8001, descripcion: "JAMÓN COCIDO 200G", categoriaNombre: "FIAMBRES", precio: 3800 },
+      { codigo: 8002, descripcion: "QUESO TYBO 200G", categoriaNombre: "FIAMBRES", precio: 4200 },
+      { codigo: 8003, descripcion: "SALAME MILÁN 150G", categoriaNombre: "FIAMBRES", precio: 4100 },
+  
+      { codigo: 9001, descripcion: "BIZCOCHUELO VAINILLA 540G", categoriaNombre: "ALMACEN", precio: 2950 },
+    ];
 
-    { codigo: 5001, descripcion: "LAVANDINA 1L", categoria: "LIMPIEZA", precio: 950 },
-    { codigo: 5002, descripcion: "DETERGENTE LIMÓN 750ML", categoria: "LIMPIEZA", precio: 1100 },
-    { codigo: 6001, descripcion: "SHAMPOO SUAVE 400ML", categoria: "PERFUMERIA", precio: 2200 },
-    { codigo: 6002, descripcion: "JABÓN DE TOCADOR X3", categoria: "PERFUMERIA", precio: 1850 },
-    { codigo: 7001, descripcion: "HAMBURGUESAS CONGELADAS X4", categoria: "CONGELADOS", precio: 5200 },
-    { codigo: 7002, descripcion: "VEGETALES MIX CONGELADOS 500G", categoria: "CONGELADOS", precio: 3600 },
-    { codigo: 8001, descripcion: "JAMÓN COCIDO 200G", categoria: "FIAMBRES", precio: 3800 },
-    { codigo: 8002, descripcion: "QUESO TYBO 200G", categoria: "FIAMBRES", precio: 4200 },
-    { codigo: 8003, descripcion: "SALAME MILÁN 150G", categoria: "FIAMBRES", precio: 4100 },
-    { codigo: 9001, descripcion: "BIZCOCHUELO VAINILLA 540G", categoria: "ALMACEN", precio: 2950 },
-  ];
+    window.addEventListener("load",()=>
+      {
+        
 
-  const categoria =
-  {
-    categoriaNombre:"CONGELADOS",
-    precioPromedio:0,
-    precioMaximo:0,
-    precioMinimo:0
-  }
+        console.clear();
 
-  const ResumenDeProductos = [categoria]; 
+        /* (01) Los productos Originales */
 
-  productos.forEach(producto => 
-    {
-       let categoriaABuscar = producto.categoria;
+        console.log("--- 1 productos originales");
+        console.log(productos);
 
-       let seEncuentraCategoria = ResumenDeProductos.find(categoria => categoria.categoriaNombre === categoriaABuscar ? true:false);
+        /* (01) Buscamos Todos los productos que tenga precio mayor a 2500 pesos */
 
-       if(seEncuentraCategoria)
-       {
-            console.log("si se encuentra");
-            console.log(categoriaABuscar);
-            console.log(seEncuentraCategoria);
-       }
-       else
-       {
-            
+        console.log("--- 2 productos con precio mayor a 2500 pesos");
+
+        productos.forEach(producto => producto.precio > 2500 ? console.log(producto):'');
+
+        /* (03) Actualizar el precio de los productos de Perfumeria en un 20% */
+
+        productos.forEach(producto => 
+          {
+            if(producto.categoriaNombre === "PERFUMERIA")
+            {
+              console.log(producto.precio);
+              producto.precio = producto.precio * 1.20;
+              console.log("producto actualizado ",producto);
+            }
+          })
+
+          console.log("--- 3 productos con precios actualizados");
+          console.log(productos);
+
+        /* (04) Marcar los productos de FIAMBRERIA en oferta si 
+        su precio es menor a 2000 pesos */
+
+        productos.forEach(producto => 
+          {
+            if((producto.categoriaNombre === "FIAMBRES") && (producto.precio <= 4000))
+            {
+                producto.productoEnOferta = true;
+            }
+            else
+            {
+                producto.productoEnOferta = false;
+            }
+          })
+
+          console.log("--- 4 Marcamos los productos en oferta");
+          console.log(productos);
+
+        /* (05) Buscar productos por Descripción o por parte de la descripción */
+
+        console.log("--- 5 Buscamos los productos por nombre");
+
+        let nombreABuscar = "QUESO";
+
+        productos.forEach(producto => {
+
+          if(producto.descripcion.includes(nombreABuscar))
+          {
+              console.log(producto);
+          }
+        })
+
+        productos.forEach(producto => producto.descripcion.includes(nombreABuscar) ? console.log(producto):'');
+
+        /* (06) Totalizar la Cantidad de Productos por Categoria
+        y obtener la cantidad de productos y la sumatoria total de precios
+        */
+
+        
+        const categoria =
+        {
+          categoriaNombre:"CONGELADOS",
+          cantidadProductos:1,
+          precioTotal:1
+        }
+
+        const ResumenDeProductos = []; 
+
+
+        productos.forEach(producto => 
+        {
+          let nombreCategoriaABuscar = producto.categoriaNombre;
+
+          let categoriaEncontrada = ResumenDeProductos.find(categoria => categoria.categoriaNombre === nombreCategoriaABuscar ? true:false);
+
+          if(categoriaEncontrada)
+          {
+            categoriaEncontrada.cantidadProductos = categoriaEncontrada.cantidadProductos + 1;
+            categoriaEncontrada.precioTotal = categoriaEncontrada.precioTotal + producto.precio;
+          }
+          else
+          {
             const categoriaNueva =
             {
-                categoriaNombre:categoriaABuscar,
-                precioPromedio:producto.precio,
-                precioMaximo:0,
-                precioMinimo:0
+              categoriaNombre:nombreCategoriaABuscar,
+              cantidadProductos:0,
+              precioTotal:0
             }     
-            
             ResumenDeProductos.push(categoriaNueva);
-          
-       }
+          }
+          console.log("Vector ",ResumenDeProductos);
+        })
 
-       console.log(ResumenDeProductos);
-    
+      
+      /* (07) Filtrar todos los productos que sean BEBIDAS
+      y calcular el impuesto al valor agregado de cada uno
+      de los productos. IVA = 21% */
 
-    })
+      const vectorDeBebidas = productos.filter(producto => producto.categoriaNombre === "BEBIDAS");
+
+      console.log("--- 7 Filtramos las Bebidas");
+
+      console.log(vectorDeBebidas);
+
+      vectorDeBebidas.forEach(bebida => bebida.IVA = bebida.precio * 21/100)
+
+      console.log(vectorDeBebidas);
+
+      /* Encadenamiento de métodos */
+
+      const vectorDeBebidas2 = productos.filter(producto => producto.categoriaNombre === "BEBIDAS").forEach(Bebida => {Bebida.precio = 0});
+      console.log(vectorDeBebidas2);
+
+      /* (08) Filtrar todos los productos que 
+      NO SEAN DE ALMACEN
+      NO SEAN PANIFICADOS 
+      */
+
+      console.log("--- 8 Filtramos y mostramos los productos que NO SON DE ALMACEN, NI SON PANIFICADOS");
+
+      const productosFiltrados = productos.filter(producto => 
+        {
+          if((producto.categoriaNombre !== "ALMACEN") && (producto.categoriaNombre !== "PANIFICADOS"))
+          {
+            return true;
+          }
+      })
+
+      console.log(productosFiltrados);
+
+      /* (09) Crear un Vector de Categorias, 
+      que solamente contenga las categorias
+      de los productos que necesiten cadena de frio
+      */
+
+      const frutas = ["MANZANA", "PERA", "BANANA"];
+
+      console.log(frutas.includes("PERA"));     // true
+      console.log(frutas.includes("SANDIA"));   // false
+
+
+
+      const productosEnCadenaDeFrio = ["LACTEOS", "FIAMBRES"];
+
+      const productosSensibles = productos.filter(producto =>productosEnCadenaDeFrio.includes(producto.categoriaNombre));
+
+      console.log(productosSensibles);
+
+      /* (10) map => recorre el vector original y realiza una función de
+      transformación sobre cada elemento */
+
+      const vectorDeNumeros = [2,5,7,-1,4];
+
+      const vectorDeCuadrados = vectorDeNumeros.map(numero => numero * numero);
+
+      console.log(vectorDeCuadrados);
+
+      /* (11) map => crearemos un vector con texto para Botones */
+
+      const idContenedor1 = document.querySelector("#idContenedor1");
+
+      const textoParaBotones = ["Boton 1","Boton 2","Boton 3","Boton 4"];
+
+      const botonesParaElDOM = textoParaBotones.map(texto => 
+        {
+            let etiquetasHTML = `<input type='button' value = '${texto}'/>`;
+            return etiquetasHTML;
+        })
+
+      console.log(botonesParaElDOM);
+
+      botonesParaElDOM.forEach(boton => 
+        {
+          idContenedor1.innerHTML = idContenedor1.innerHTML + boton;
+        })
+
+
+      /* (12) map => convirtiendo los productos en una etiqueta HTML */
+
+      const etiquetasHTML = productos.map(producto => 
+        {
+            let salida = `<h3>codigo: ${producto.codigo} - nombre: ${producto.descripcion} - precio: ${producto.precio} </h3>`;
+
+            return salida;
+
+        })
+
+      console.log(etiquetasHTML);
+
+      etiquetasHTML.forEach(etiqueta => 
+        {
+          idContenedor1.innerHTML = idContenedor1.innerHTML + etiqueta;
+
+        })
+      
+      
+      
+
+
+
+
+
+      })
+
+
+
 
